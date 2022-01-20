c89cf031a56717d610385552cd4324d9c8c93e35
Dima.mihaylov <playcssv34@gmail.com>
2022-01-20 12:11:51 +0300

    create item for catalog

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 900,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../icon/chevron-left-solid.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icon/chevron-right-solid.png"></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="icon/chevron-left-solid.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icon/chevron-right-solid.png"></button>',
         responsive: [
             {
               breakpoint: 992,
               settings: {
                 arrows: false,
-                dots: true
+                dots: false
               }
             }
           ]
